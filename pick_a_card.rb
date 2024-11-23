9703058894b834e0069e187e1edbc8e3578358c1
daniil <daniil.onish@gmail.com>
2024-11-23 19:51:31 +0200

    ver2

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'B', 'Q', 'K', 'T']
-suits = ['Buba', 'Churva', 'Kresta', 'Pika']
+suits = ['♦', '♥', '♣', '♠']
 
-puts "#{values.sample} of #{suits.sample}" 
+puts "#{values.sample}#{suits.sample}" 
